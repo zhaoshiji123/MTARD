bd4a912c34aee1bdd54b66387f238ef56f9c1235
zhaoshiji123 <47933549+zhaoshiji123@users.noreply.github.com>
2023-09-22 10:03:06 +0800

    Add files via upload

---
+++ cifar10_nat_teacher_models/__init__.py
@@ -0,0 +1,61 @@
+from .resnet import cifar10_resnet20
+from .resnet import cifar10_resnet32
+from .resnet import cifar10_resnet44
+from .resnet import cifar10_resnet56
+
+from .resnet import cifar100_resnet20
+from .resnet import cifar100_resnet32
+from .resnet import cifar100_resnet44
+from .resnet import cifar100_resnet56
+
+from .vgg import cifar10_vgg11_bn
+from .vgg import cifar10_vgg13_bn
+from .vgg import cifar10_vgg16_bn
+from .vgg import cifar10_vgg19_bn
+
+from .vgg import cifar100_vgg11_bn
+from .vgg import cifar100_vgg13_bn
+from .vgg import cifar100_vgg16_bn
+from .vgg import cifar100_vgg19_bn
+
+from .mobilenetv2 import cifar10_mobilenetv2_x0_5
+from .mobilenetv2 import cifar10_mobilenetv2_x0_75
+from .mobilenetv2 import cifar10_mobilenetv2_x1_0
+from .mobilenetv2 import cifar10_mobilenetv2_x1_4
+
+from .mobilenetv2 import cifar100_mobilenetv2_x0_5
+from .mobilenetv2 import cifar100_mobilenetv2_x0_75
+from .mobilenetv2 import cifar100_mobilenetv2_x1_0
+from .mobilenetv2 import cifar100_mobilenetv2_x1_4
+
+from .shufflenetv2 import cifar10_shufflenetv2_x0_5
+from .shufflenetv2 import cifar10_shufflenetv2_x1_0
+from .shufflenetv2 import cifar10_shufflenetv2_x1_5
+from .shufflenetv2 import cifar10_shufflenetv2_x2_0
+
+from .shufflenetv2 import cifar100_shufflenetv2_x0_5
+from .shufflenetv2 import cifar100_shufflenetv2_x1_0
+from .shufflenetv2 import cifar100_shufflenetv2_x1_5
+from .shufflenetv2 import cifar100_shufflenetv2_x2_0
+
+from .repvgg import cifar10_repvgg_a0
+from .repvgg import cifar10_repvgg_a1
+from .repvgg import cifar10_repvgg_a2
+
+from .repvgg import cifar100_repvgg_a0
+from .repvgg import cifar100_repvgg_a1
+from .repvgg import cifar100_repvgg_a2
+
+from .vit import cifar10_vit_b16
+from .vit import cifar10_vit_b32
+from .vit import cifar10_vit_l16
+from .vit import cifar10_vit_l32
+from .vit import cifar10_vit_h14
+
+from .vit import cifar100_vit_b16
+from .vit import cifar100_vit_b32
+from .vit import cifar100_vit_l16
+from .vit import cifar100_vit_l32
+from .vit import cifar100_vit_h14
+
+__version__ = "0.1.0-alpha"
